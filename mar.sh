047c94a23b986f830f3a7baecd8f8141cfed90c9
AFRcloud <rohendimtq246@gmail.com>
2025-07-16 20:36:26 +0700

    Update mar.sh

--- mar.sh
+++ mar.sh
@@ -123,10 +123,10 @@ timedatectl set-timezone Asia/Jakarta;
 sudo bash -c "$(curl -sL https://github.com/GawrAme/Marzban-scripts/raw/master/marzban.sh)" @ install
 
 #Install Subs
-wget -N -P /var/lib/marzban/templates/subscription/  https://raw.githubusercontent.com/Nizwarax/Killer/main/index.html
+wget -N -P /var/lib/marzban/templates/subscription/  https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/index.html
 
 #install env
-wget -O /opt/marzban/.env "https://raw.githubusercontent.com/Nizwarax/Killer/main/env"
+wget -O /opt/marzban/.env "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/env"
 
 #install Assets folder
 mkdir -p /var/lib/marzban/assets
@@ -134,20 +134,20 @@ cd
 
 #profile
 echo -e 'profile' >> /root/.profile
-wget -O /usr/bin/profile "https://raw.githubusercontent.com/Nizwarax/Killer/main/profile";
+wget -O /usr/bin/profile "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/profile";
 chmod +x /usr/bin/profile
 apt install neofetch -y
-wget -O /usr/bin/cekservice "https://raw.githubusercontent.com/Nizwarax/Killer/main/cekservice.sh"
+wget -O /usr/bin/cekservice "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/cekservice.sh"
 chmod +x /usr/bin/cekservice
 
 #install compose
-wget -O /opt/marzban/docker-compose.yml "https://raw.githubusercontent.com/Nizwarax/Killer/main/docker-compose.yml"
+wget -O /opt/marzban/docker-compose.yml "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/docker-compose.yml"
 
 #Install VNSTAT
 apt -y install vnstat
 /etc/init.d/vnstat restart
 apt -y install libsqlite3-dev
-wget https://github.com/Nizwarax/Killer/raw/main/vnstat-2.6.tar.gz
+wget https://github.com/AFRcloud/AFRcloud-Marzban/raw/main/vnstat-2.6.tar.gz
 tar zxvf vnstat-2.6.tar.gz
 cd vnstat-2.6
 ./configure --prefix=/usr --sysconfdir=/etc && make && make install 
@@ -166,9 +166,9 @@ sudo apt-get install speedtest -y
 mkdir -p /var/log/nginx
 touch /var/log/nginx/access.log
 touch /var/log/nginx/error.log
-wget -O /opt/marzban/nginx.conf "https://raw.githubusercontent.com/Nizwarax/Killer/main/nginx.conf"
-wget -O /opt/marzban/default.conf "https://raw.githubusercontent.com/Nizwarax/Killer/main/vps.conf"
-wget -O /opt/marzban/xray.conf "https://raw.githubusercontent.com/Nizwarax/Killer/main/xray.conf"
+wget -O /opt/marzban/nginx.conf "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/nginx.conf"
+wget -O /opt/marzban/default.conf "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/vps.conf"
+wget -O /opt/marzban/xray.conf "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/xray.conf"
 mkdir -p /var/www/html
 echo "<pre>Setup by AutoScript KillerVPN</pre>" > /var/www/html/index.html
 
@@ -181,7 +181,7 @@ apt install socat cron bash-completion -y
 curl https://get.acme.sh | sh -s email=$email
 /root/.acme.sh/acme.sh --server letsencrypt --register-account -m $email --issue -d $domain --standalone -k ec-256 --debug
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /var/lib/marzban/xray.crt --keypath /var/lib/marzban/xray.key --ecc
-wget -O /var/lib/marzban/xray_config.json "https://raw.githubusercontent.com/Nizwarax/Killer/main/xray_config.json"
+wget -O /var/lib/marzban/xray_config.json "https://raw.githubusercontent.com/AFRcloud/AFRcloud-Marzban/main/xray_config.json"
 
 #install firewall
 apt install ufw -y
@@ -196,7 +196,7 @@ sudo ufw allow 1080/udp
 yes | sudo ufw enable
 
 #install database
-wget -O /var/lib/marzban/db.sqlite3 "https://github.com/Nizwarax/Killer/raw/main/db.sqlite3"
+wget -O /var/lib/marzban/db.sqlite3 "https://github.com/AFRcloud/AFRcloud-Marzban/raw/main/db.sqlite3"
 
 #install WARP Proxy
 wget -O /root/warp "https://raw.githubusercontent.com/hamid-gh98/x-ui-scripts/main/install_warp_proxy.sh"
